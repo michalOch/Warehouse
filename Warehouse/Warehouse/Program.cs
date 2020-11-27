d77a8e0734621b29d4cb04607c4f0174b4aa0e9b
michalOch <ochrobot1990@gmail.com>
2020-11-27 21:39:45 +0100

    create project roadmap

--- Warehouse/Warehouse/Program.cs
+++ Warehouse/Warehouse/Program.cs
@@ -6,7 +6,28 @@ namespace Warehouse
     {
         static void Main(string[] args)
         {
-            
+            // Użytkownik zostanie przywitany
+            // Dostanie możliwość wyboru akcji 
+            ////a. Stworzenie nowego przedmiotu
+            ////b. Usunięcie przedmiotu
+            ////c. Sprawdzenie stanu magazynowego
+            ////d. Zwrócenie listy przedmiotów o zadanym filtrze (nazwa kategorii)
+
+            //// a1 Najpierw dostane do wybory kategorię produktu
+            //// a2 Zostanę poproszony o wprowadzenie wszystkich szczegółów dotyczących produktu
+
+            //// b1 Zostanę poproszony o id lub nazwę produktu
+            //// b2 Usunę ten produkt z listy produktów
+
+            //// c1 Zostanę poproszony o wprowadzenie id produktu
+            //// c2 Wyświetle wszystkie informacje związane z tym produktem
+
+            //// d1 Zostanę poproszony o wprowadzenie nazwy albo id kategorii produktów
+            //// d2 Wyświetlę listę produktów
+
+
+            Console.WriteLine("Welcome to warehouse app!");
+            Console.WriteLine("Please let me know what you want to do: ");
         }
     }
 }
