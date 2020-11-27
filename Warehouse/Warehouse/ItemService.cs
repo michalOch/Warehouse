46bce13c278dc7ab23ad7794d3d12f021df08155
michalOch <ochrobot1990@gmail.com>
2020-11-27 22:20:59 +0100

    add Item and ItemService class

---
+++ Warehouse/Warehouse/ItemService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse
+{
+    public class ItemService
+    {
+        public List<Item> Items { get; set; }
+        public ItemService()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
