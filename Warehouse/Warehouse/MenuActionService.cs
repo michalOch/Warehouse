576207626642e29bef1733fe1764a98904f99ac3
michalOch <ochrobot1990@gmail.com>
2020-11-27 21:40:55 +0100

    add MenuAction and MenuActionService class

---
+++ Warehouse/Warehouse/MenuActionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse
+{
+    public class MenuActionService
+    {
+        private List<MenuAction> menuActions;
+
+        public void AddNewaction(int id, string name, string menuName)
+        {
+            MenuAction menuAction = new MenuAction() { Id = id, Name = name, MenuName = menuName };
+            menuActions.Add(menuAction);
+        }
+    
+        public List<MenuAction> GetMenuActionsByMenuName(string menuName)
+        {
+            List<MenuAction> result = new List<MenuAction>();
+            foreach(var menuAction in menuActions)
+            {
+                if(menuAction.MenuName == menuName)
+                {
+                    result.Add(menuAction);
+                }
+            }
+            return result;
+        }
+    }
+}
